8b2b1f89f3a03e5602d67ea09ea5fe1aab05721a
ibrahim.a.y.m.98@gmail.com <ibrahim.a.y.m.98@gmail.com>
2024-11-18 09:45:41 +0300

    tidying Repo

--- Stock-Copy/demo/src/main/java/com/example/demo/repository/StockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.StockEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StockRepository extends CrudRepository<StockEntity, Long> {
-    Iterable<StockEntity> findByItem(String item);
-}
